20e9e9e670c118702a1178a5fbe4afb4f08fcd12
Kainar <toleubaevkai@mail.ru>
2020-12-07 13:20:31 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+import slider from './slider.js';
+
+slider({
+    container: '.offer__slider',
+    nextArrow: '.offer__slider-next',
+    prevArrow: '.offer__slider-prev',
+    slide: '.offer__slide',
+    totalCounter: '#total',
+    currentCounter: '#current',
+    wrapper: '.offer__slider-wrapper',
+    field: '.offer__slider-inner'
+});
